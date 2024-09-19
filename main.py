7fea97557ca3528775aad30316ab70e86e9556e6
Calvin <cal@Calvins-MacBook-Pro.local>
2024-09-19 10:08:08 -0600

    add functions to refresh token, send buy order

--- main.py
+++ main.py
@@ -1,92 +1,10 @@
-# Schwab Auto Trader
-# To use this app one must have an schwab developer account. 
+from flask import Flask
 
-# Imports
-import os
-import base64
-import requests
-import webbrowser
-from loguru import logger
+app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return "hi"
 
-def construct_init_auth_url() -> tuple[str, str, str]:
-    
-    app_key = "your-app-key"
-    app_secret = "your-app-secret"
-
-    auth_url = f"https://api.schwabapi.com/v1/oauth/authorize?client_id={app_key}&redirect_uri=https://127.0.0.1"
-
-    logger.info("Click to authenticate:")
-    logger.info(auth_url)
-
-    return app_key, app_secret, auth_url
-
-def construct_headers_and_payload(returned_url, app_key, app_secret):
-    response_code = f"{returned_url[returned_url.index('code=') + 5: returned_url.index('%40')]}@"
-
-    credentials = f"{app_key}:{app_secret}"
-    base64_credentials = base64.b64encode(credentials.encode("utf-8")).decode(
-        "utf-8"
-    )
-
-    headers = {
-        "Authorization": f"Basic {base64_credentials}",
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-
-    payload = {
-        "grant_type": "authorization_code",
-        "code": response_code,
-        "redirect_uri": "https://127.0.0.1",
-    }
-
-    return headers, payload
-
-def retrieve_tokens(headers, payload) -> dict:
-    init_token_response = requests.post(
-        url="https://api.schwabapi.com/v1/oauth/token",
-        headers=headers,
-        data=payload,
-    )
-
-    init_tokens_dict = init_token_response.json()
-
-    return init_tokens_dict
-
-def main():
-    app_key, app_secret, cs_auth_url = construct_init_auth_url()
-    webbrowser.open(cs_auth_url)
-
-    logger.info("Paste Returned URL:")
-    returned_url = input()
-
-    init_token_headers, init_token_payload = construct_headers_and_payload(
-        returned_url, app_key, app_secret
-    )
-
-    init_tokens_dict = retrieve_tokens(
-        headers=init_token_headers, payload=init_token_payload
-    )
-
-    logger.debug(init_tokens_dict)
-
-    return "Done!"
-
-def get_data():
-
-    # Send request to get QQQ data
-    response = requests.get(url, headers=headers)
-
-    #Check if the request was successful
-    if response.status_code == 200:
-        data = response.json()
-        # Assuming the price is under 'lastPrice' key in the response
-        last_price = data.get('lastPrice', 'N/A')
-        print(f'The current price of QQQ is: ${last_price}')
-    else:
-        print(f'Failed to retrieve data: {response.status_code}')
-
-        
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
